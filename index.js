26737502caa680f774da3e19ba67cea187e5728d
Brenda <waiyegobrenda99@gmail.com>
2022-08-21 22:19:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 document.getElementById("main").remove();
 
 let newHeader = document.createElement('h1');
+
 document.body.appendChild(newHeader)
+
 newHeader.setAttribute( "id", "victory")
+
 newHeader.innerHTML= 'Bree is the champion';
